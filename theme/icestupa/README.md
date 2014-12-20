40f60bc8a3aa1518d9f102a267f72e1c054dee5c
smhigley <sarahmhigley@gmail.com>
2014-12-19 20:44:21 -0600

    content types and initial theme setup

---
+++ theme/icestupa/README.md
@@ -0,0 +1,4 @@
+Ice Stupa Theme
+=========
+
+This is a custom theme for the Ice Stupa Project, design for the Bolt CMS
---
+++ theme/icestupa/gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    minifyCSS = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    rename = require('gulp-rename'),
+    size = require('gulp-size'),
+    pkg = require('./package.json');
+
+var paths = {
+  styles: './css/style.scss',
+  scripts: ['./bower_components/jquery/dist/jquery.min.js', './js/main.js']
+};
+
+gulp.task('styles', ['components'], function () {
+  gulp.src(paths.styles)
+    .pipe(sass({ style: 'expanded' })) // Add source maps after figuring out minify issue
+    .pipe(gulp.dest('./css/'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifyCSS())
+    .pipe(size())
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('components', function() {
+  return gulp.src(['./bower_components/normalize.css/normalize.css'])
+  .pipe(rename('_normalize.scss'))
+  .pipe(gulp.dest('./bower_components/normalize.css/'));
+});
+
+gulp.task('scripts', ['lint'], function() {
+  gulp.src(paths.scripts)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./js/'))
+    .pipe(rename('app.min.js'))
+    .pipe(uglify({ preserveComments: 'some' }))
+    .pipe(size())
+    .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('lint', function () {
+  return gulp.src('./js/main.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  gulp.watch('./js/*.js', ['scripts']);
+  gulp.watch('./css/*.scss', ['styles']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['styles', 'scripts']);
